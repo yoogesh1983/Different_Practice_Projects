7148f4f63a762170963ad96dba83bca93e01c165
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2020-12-20 14:51:52 -0500

    Update models.py

--- python/01_Django/YoogehPythonProject/myaccount/twmprofile/models.py
+++ python/01_Django/YoogehPythonProject/myaccount/twmprofile/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     author=models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
     body= models.TextField()
     publish=models.DateTimeField(default=timezone.now)
-    created=models.DateTimeField(auto_now_add=True) # when it is updated that time is taken
+    created=models.DateTimeField(auto_now_add=True) # everytime the post object is updated that time is taken
     updated = models.DateTimeField(auto_now=True) # when save method is called that time is taken
     status=models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
@@ -22,9 +22,9 @@ class Post(models.Model):
         # only one field (because there must not be a single valued tuple)
         ordering=('-publish',)
 
-    def get_absolute_url(self):
-        return reverse('detail',kwargs={'pk':self.pk})
-
     def __str__(self):
         return 'title: ' + self.username + ' | author: ' + self.author
+
+    def get_absolute_url(self):
+        return reverse('detail',kwargs={'pk':self.pk})
 
