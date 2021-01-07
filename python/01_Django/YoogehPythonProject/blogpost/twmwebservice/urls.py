5a7fc1cb5630d302c73c1e096a60ef73754a5506
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2021-01-06 19:47:50 -0500

    udpate

--- python/01_Django/YoogehPythonProject/blogpost/twmwebservice/urls.py
+++ python/01_Django/YoogehPythonProject/blogpost/twmwebservice/urls.py
@@ -10,6 +10,14 @@ urlpatterns = [
     path('cbv/post/', views.PostListCBV.as_view()),
     re_path('cbv/post/(?P<id>\d+)/$', views.PostDetailCBV.as_view()),
 
-    path('drf/', views.PostCreateAPIView_ShortCutWay.as_view()),
+    # path('drf/', views.PostCreateAPIView_ShortCutWay.as_view()),
+    # re_path('drf/(?P<id>\d+)/$', views.PostRetrieveAPIView_ShortCutWay.as_view()),
+    # re_path('drf/(?P<id>\d+)/$', views.PostUpdateAPIView_ShortCutWay.as_view()),
+    # re_path('drf/(?P<id>\d+)/$', views.PostDestroyAPIView_ShortCutWay.as_view()),
+
+    # path('drf/$', views.PostListAndCreateAPIView_ShortCutWay.as_view()),
+    # re_path('drf/(?P<id>\d+)/$', views.PostRetrieveAndUpdateAPIView_ShortCutWay.as_view()),
+    # re_path('drf/(?P<id>\d+)/$', views.PostRetrieveAndDestroyAPIView_ShortCutWay.as_view()),
+    re_path('drf/(?P<id>\d+)/$', views.PostRetrieveUpdateAndDestroyAPIView_ShortCutWay.as_view()),
 
     ]
--- python/01_Django/YoogehPythonProject/blogpost/twmwebservice/views.py
+++ python/01_Django/YoogehPythonProject/blogpost/twmwebservice/views.py
@@ -6,7 +6,8 @@ from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveDestroyAPIView, \
+    RetrieveUpdateAPIView, ListCreateAPIView, DestroyAPIView, UpdateAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from twmblog.forms import AddPostRequest
@@ -168,9 +169,84 @@ class PostCreateAPIView_ShortCutWay(CreateAPIView):
     serializer_class = PostSerializer
 
 
+########################################################################################
+# Using Django framework API-View
+########################################################################################
+
+class PostListAPIView(APIView):
+    def get(self, request, format=None, *args, **kwargs):
+        queryset = Post.objects.all()
+        pk = self.request.GET.get('id')  # when you pass /?id=4
+        if pk is not None:
+            queryset = queryset.filter(id__iexact=pk)
+        serializer = PostSerializer(queryset, many=True)
+        dict_data = serializer.data
+        # The Response method internally convert the Python_dictionary data into Json and return the Json response to the end user
+        return Response(dict_data)
 
 
+class PostListAPIView_ShortCutWay(ListAPIView):
+    # queryset = Post.objects.all()    # queryset is defined and cannot be other that it. require here if we don't override get_queryset() method
+    serializer_class = PostSerializer  # serializer_class is defined and cannot be other
 
+    # If you want the customized list as a response then you need to override this.
+    # It is used especially to implement search operation
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        pk = self.request.GET.get('id')  # when you pass /?id=4
+        if pk is not None:
+            queryset = queryset.filter(id__iexact=pk)
+        return queryset
+
+
+class PostCreateAPIView_ShortCutWay(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostRetrieveAPIView_ShortCutWay(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'  # default lookup field is 'pk'
+
+
+class PostUpdateAPIView_ShortCutWay(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'  # default lookup field is 'pk'
+
+
+class PostDestroyAPIView_ShortCutWay(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'  # default lookup field is 'pk'
+
+#####################################################################
+############################### List and Create both at the same class ####
+class PostListAndCreateAPIView_ShortCutWay(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    def get_queryset(self):
+        queryset = Post.objects.all()
+        pk = self.request.GET.get('id')  # when you pass /?id=4
+        if pk is not None:
+            queryset = queryset.filter(id__iexact=pk)
+        return queryset
+
+class PostRetrieveAndUpdateAPIView_ShortCutWay(RetrieveUpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'  # default lookup field is 'pk'
+
+class PostRetrieveAndDestroyAPIView_ShortCutWay(RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'  # default lookup field is 'pk'
+
+class PostRetrieveUpdateAndDestroyAPIView_ShortCutWay(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_field = 'id'  # default lookup field is 'pk'
 
 
 
