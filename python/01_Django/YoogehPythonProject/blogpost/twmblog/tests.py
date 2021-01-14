e4960fd1e28f39d6f9e8f7f07fc65f71224e9668
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2021-01-14 08:27:51 -0500

    testing

--- python/01_Django/YoogehPythonProject/blogpost/twmblog/tests.py
+++ python/01_Django/YoogehPythonProject/blogpost/twmblog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from twmblog.models import EmailForm, Comment, Post
 
-# Create your tests here.
+
+class PostTestCase(TestCase):
+
+    # Overide
+    def setUp(self):
+        print('init() method called......')
+        # Post.objects.create(title='test1', slug='test', author=1)
+
+    def test_PostInfo(self):
+        print('PostInfo() method called......')
+        qs = Post.objects.all()
+        self.assertEqual(qs.count(), 0)
+        # p1 = Post.objects.get(title='test1')
+        # self.assertEqual(p1.slug == 'test')
+
+    def tearDown(self):
+        print('Destroy() method called.....')
--- python/01_Django/YoogehPythonProject/blogpost/twmwebservice/tests.py
+++ python/01_Django/YoogehPythonProject/blogpost/twmwebservice/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from twmblog.models import EmailForm, Comment, Post
 
-# Create your tests here.
+class PostAPITestCase(APITestCase):
+    def setUp(self):
+        print('init() API method called.....')
+
+    def test_getPosts(self):
+        print('API test Service() method called.....')
+        url='http://127.0.0.1:8000/api/drf/apiviews/core/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        qs = Post.objects.filter(slug= 'totalwine')
+        self.assertEqual(qs.count(), 0)
+
+    def tearDown(self):
+        print('destroy() API method called...')
+
+
